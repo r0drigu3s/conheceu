92eb2b9d126be750761add18c7db5407e17b9b3e
Lucas <lucasr4394@gmail.com>
2022-07-15 17:41:13 -0300

    atualizações na estrutura

---
+++ src/components/Rodape.vue
@@ -0,0 +1,38 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+<div class="container ">
+<v-footer class="d-flex justify-center"> 
+      <v-card flat tile class="black lighten-1 white--text text-center">
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+          >
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-divider class="white"></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Alumiô Bazar</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+</div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Rodape",
+
+  data: () => ({
+    icons: ["mdi-facebook", "mdi-instagram"],
+  }),
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <header></header>
+
+    <v-main>
+      <form-conheceu />
+    </v-main>
+
+    <v-footer>
+      <rodape />
+    </v-footer>
+  </div>
+</template>
+
+<script>
+import FormConheceu from "../components/FormConheceu";
+import Rodape from "../components/Rodape";
+
+export default {
+  name: "App",
+
+  components: {
+    FormConheceu,
+    Rodape,
+  },
+};
+</script>
